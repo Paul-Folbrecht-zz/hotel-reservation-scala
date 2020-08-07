35c764ac36b84874bc9578032a0603352b3184a1
Pam Folbrecht <pamfolbrecht@Pams-MacBook-Air.local>
2020-08-06 23:34:30 -0500

    1.0

--- src/main/scala/com/bravolt/Hotel.scala
+++ src/main/scala/com/bravolt/Hotel.scala
@@ -1,7 +1,57 @@
 package com.bravolt
 
-case class Room(capacity: Int, pets: Boolean)
+import java.util.Date
 
-class Hotel {
+case class Room(beds: Int, floor: Int)
 
+case class ReservationRequest(beds: Int,
+                              arrival: Date,
+                              departure: Date,
+                              pets: Int = 0,
+                              handicapAccessible: Boolean = false)
+
+class Hotel(private val rooms: Seq[Room]) {
+
+  private object EventType extends Enumeration {
+    val Arrival: Value = Value("Arrival")
+    val Departure: Value = Value("Departure")
+  }
+
+  // Make a seq of (date, Event (Arrival/Departure)) tuples, sorted by date
+  // Iterate up to arrival date of incoming reservation, count + 1 for arrivals, -1 for departures
+  // eligableRooms = all rooms filtered on reservation criteria
+  // If eligableRooms.size > occupancyCount, success
+  def reserve(request: ReservationRequest,
+              existingRequests: Seq[ReservationRequest]): Boolean = {
+    // Should an existing reservation "count against" the request? It should if the existing reservation requires a room
+    // with the same criteria.
+    def requestPredicate(existingRequest: ReservationRequest): Boolean = {
+      val floor = if (requiresFirstFloor(existingRequest)) 1 else 2 // @todo Use a placeholder of sorts for floors other than 1
+      roomPredicate(Room(existingRequest.beds, floor))
+    }
+
+    def roomPredicate(room: Room): Boolean = {
+      // has pet or handicap, room fl 1 == (!true || true) = true
+      // has pet or handicap, room fl 2 == (!true || false) = false
+      // no pet or handicap, room fl 1  == (!false || true) = true
+      // no pet or handicap, room fl 2  == (!false || false) = true
+      if (request.pets > 2) false
+      else request.beds <= room.beds && (!requiresFirstFloor(request) || room.floor == 1)
+    }
+
+    val events: Seq[(Date, EventType.Value)] = existingRequests
+      .filter(requestPredicate)
+      .foldLeft(Seq[(Date, EventType.Value)]()) { case (seq, request) =>
+        seq ++ Seq((request.arrival, EventType.Arrival), (request.departure, EventType.Departure))
+      }.filter(existingRequest => coincident(existingRequest._1, request.arrival)).sortBy(_._1)
+    val occupancyCount: Int = events.foldLeft(0) { case (count, event) =>
+      if (event._2 == EventType.Arrival) count + 1 else count - 1
+    }
+    val eligableRooms = rooms.filter(roomPredicate)
+    eligableRooms.size > occupancyCount
+  }
+
+  private def requiresFirstFloor(request: ReservationRequest): Boolean = request.pets > 0 || request.handicapAccessible
+
+  private def coincident(one: Date, two: Date): Boolean = (one.before(two) || one.equals(two))
 }
--- src/test/scala/com/bravolt/HotelSpec.scala
+++ src/test/scala/com/bravolt/HotelSpec.scala
@@ -1,11 +1,103 @@
 package com.bravolt
 
+import java.text.SimpleDateFormat
+
 import org.scalatest.flatspec._
 
 class HotelSpec extends AnyFlatSpec {
+  private val dateFormatter = new SimpleDateFormat("dd/MM/yyyy")
+  private val rooms = Seq(
+    Room(1, 1),
+    Room(2, 1),
+    Room(2, 1),
+    Room(2, 1),
+    Room(3, 2),
+    Room(3, 2)
+  )
+
   behavior of "Our reservation system"
 
-  it should "have size 0" in {
+  it should "" in {
     assert(Set.empty.size === 0)
+  }
+
+  it should "allow valid reservations" in {
+    val existingRequests = Seq(
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/5/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/10/2020"))
+    )
+    val request = ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020"))
+    assert(new Hotel(rooms).reserve(request, existingRequests))
+  }
+
+  it should "allow valid reservations corner case" in {
+    val existingRequests = Seq(
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/5/2020"))
+    )
+    val request = ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020"))
+    assert(new Hotel(rooms).reserve(request, existingRequests))
+  }
+
+  it should "reject overbooking reservations" in {
+    val existingRequests = Seq(
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/5/2020")),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/5/2020"))
+    )
+    val request = ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/8/2020"))
+    assert(!new Hotel(rooms).reserve(request, existingRequests))
+  }
+
+  it should "respect occupancy requirements" in {
+    val existingRequests = Seq(
+      ReservationRequest(3, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(3, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020")),
+      ReservationRequest(3, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/5/2020"))
+    )
+    val request = ReservationRequest(3, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/8/2020"))
+    assert(!new Hotel(rooms).reserve(request, existingRequests))
+  }
+
+  it should "respect occupancy requirements 2" in {
+    val existingRequests = Seq()
+    val request = ReservationRequest(4, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/8/2020"))
+    assert(!new Hotel(rooms).reserve(request, existingRequests))
+  }
+
+  it should "respect pet requirements" in {
+    val existingRequests = Seq(
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020"), 1),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020"), 1),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020"), 1),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020"), 1)
+    )
+    val request = ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/8/2020"), 1)
+    assert(!new Hotel(rooms).reserve(request, existingRequests))
+  }
+
+  it should "respect max pet requirements" in {
+    val existingRequests = Seq()
+    val request = ReservationRequest(4, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/8/2020"), 3)
+    assert(!new Hotel(rooms).reserve(request, existingRequests))
+  }
+
+  it should "respect handicap access requirements" in {
+    val existingRequests = Seq(
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020"), 0, handicapAccessible = true),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020"), 0, handicapAccessible = true),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020"), 0, handicapAccessible = true),
+      ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/3/2020"), 0, handicapAccessible = true)
+    )
+    val request = ReservationRequest(1, dateFormatter.parse("1/1/2020"), dateFormatter.parse("1/8/2020"), 0, handicapAccessible = true)
+    assert(!new Hotel(rooms).reserve(request, existingRequests))
   }
 }
